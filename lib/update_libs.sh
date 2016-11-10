e4ac3ae51af05d0db5314ab71e901687a6d29ab0
EmekaNkurumeh <emekankurumeh@outlook.com>
2016-11-10 00:30:27 -0600

    fixed stalker update

--- lib/update_libs.sh
+++ lib/update_libs.sh
@@ -11,6 +11,6 @@ wget -nv -N "https://raw.githubusercontent.com/rxi/flux/master/flux.lua"
 wget -nv -N "https://raw.githubusercontent.com/rxi/lume/master/lume.lua"
 wget -nv -N "https://raw.githubusercontent.com/rxi/tick/master/tick.lua"
 wget -nv -N "https://raw.githubusercontent.com/kikito/bump.lua/master/bump.lua"
-wget -nv -N "https://gist.githubusercontent.com/EmekaNkurumeh/8038aef0f10e635857c0c2f8338c2696/raw/a24895c0b3c67d4792732005171597d15a926dea/stalker.lua"
+# wget -nv -N "https://gist.githubusercontent.com/EmekaNkurumeh/8038aef0f10e635857c0c2f8338c2696/raw/a24895c0b3c67d4792732005171597d15a926dea/stalker.lua"
 
 echo "Done"
